a9b78fa8efe9162f6eb7c17f2e5102b15f0f7af8
Ryosuke Minami <contact@ryosuke.dev>
2024-01-16 00:58:30 +0900

    update homebrew installation

--- macos_setup.sh
+++ macos_setup.sh
@@ -2,7 +2,7 @@
 # xcode-select --install
 
 # Install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Configure git
 git config --global user.name "Ryosuke Minami"

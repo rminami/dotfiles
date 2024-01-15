6c9a42f875b814f045b6d0049479afdb7e66a4c7
Ryosuke Minami <contact@ryosuke.dev>
2024-01-16 00:27:56 +0900

    remove unnecessary packages

--- macos_setup.sh
+++ macos_setup.sh
@@ -9,7 +9,6 @@ git config --global user.name "Ryosuke Minami"
 git config --global user.email "ryosukem13@gmail.com"
 
 # Install essential command line tools
-brew install zsh
 brew install neovim
 brew install wget
 brew install tree
@@ -18,32 +17,6 @@ brew install the_silver_searcher
 brew install mas
 brew install tmux
 brew install bat
-brew install topgrade
-
-# Install MacOS applications
-brew cask install iterm2
-brew cask install dropbox
-brew cask install sublime-text
-brew cask install google-chrome
-brew cask install google-japanese-ime
-brew cask install spotify
-brew cask install visual-studio-code
-brew cask install atom
-brew cask install vlc
-brew cask install iina
-brew cask install google-backup-and-sync
-brew cask install android-file-transfer
-brew cask install pocket-casts
-brew cask install firefox
-
-# Install Mac Apps from App Store
-mas install 918858936 # Airmail
-mas install 775737590 # iA Writer
-mas install 425424353 # The Unarchiver
-
-# Install language packs
-brew install node
-brew install yarn
 
 # Install npm packages
 npm install -g serve
